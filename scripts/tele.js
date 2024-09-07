21f3e7abc941a773cce25556c86092c9cc52a671
vunguyen <vunguyen1987>
2024-09-08 00:00:34 +0700

    jsinterop

--- scripts/tele.js
+++ scripts/tele.js
@@ -1,18 +1,49 @@
-var tonConnectUI
+var WalletSvc
 
 window.onload = function(){
-    tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+    WalletSvc = new WalletService()
+    WalletSvc.initEvent()
+}
+
+async function connectOpenFort() {
+    resp = await fetch("https://api.doschain.com/game/config");
+    payload = await resp.json()
+    return evt(JSON.stringify(payload))
+}
+
+function WalletService() {
+    this.tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
         manifestUrl: 'https://ecraftagency.github.io/tonconnect-manifest.json'
     });
 }
+WalletService.prototype.initEvent = function() {
+    window.addEventListener('ton-connect-ui-connection-completed', (event) => {
+        console.log('wallet connected', event);
+        Web3Events.onWalletConnected(JSON.stringify(event));
+    });
 
-async function connectOpenFort(evt) {
-    resp = await fetch("https://api.doschain.com/game/config");
-    payload = await resp.json()
-    evt(JSON.stringify(payload))
+    window.addEventListener('ton-connect-ui-connection-started', (event) => {
+            console.log('wallet connected', event);
+            Web3Events.onWalletConnectStarted(JSON.stringify(event));
+        });
+
+    window.addEventListener('ton-connect-ui-disconnection', (event) => {
+        console.log('wallet disconnected', event);
+        Web3Events.onWalletDisconnected(JSON.stringify(event));
+    });
 }
 
-async function openTonModal() {
+WalletService.prototype.openConnectPopup = async function() {
+    await this.tonConnectUI.openModal();
+}
 
-    await tonConnectUI.openModal();
+WalletService.prototype.wallet = function() {
+    if (this.tonConnectUI.wallet == null) {
+        return ""
+    }
+    return JSON.stringify(this.tonConnectUI.wallet)
+}
+
+WalletService.prototype.isConnected = function() {
+    return this.tonConnectUI.connected
 }
